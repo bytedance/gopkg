96e6bc122957b5e4ba569007c47c2f8de13f6112
zhangyunhao <zhangyunhao116@gmail.com>
2021-06-22 10:56:37 +0800

    fix go vet warning

--- collection/lscq/asm.s
+++ collection/lscq/asm.s
@@ -18,10 +18,10 @@
 
 TEXT ·compareAndSwapUint128(SB),NOSPLIT,$0
 	MOVQ addr+0(FP), R8
-	MOVQ old+8(FP), AX
-	MOVQ old+16(FP), DX
-	MOVQ new+24(FP), BX
-	MOVQ new+32(FP), CX
+	MOVQ old_0+8(FP), AX
+	MOVQ old_1+16(FP), DX
+	MOVQ new_0+24(FP), BX
+	MOVQ new_1+32(FP), CX
 	LOCK
 	CMPXCHG16B (R8)
 	SETEQ swapped+40(FP)
@@ -35,8 +35,8 @@ TEXT ·loadUint128(SB),NOSPLIT,$0
 	XORQ CX, CX
 	LOCK
 	CMPXCHG16B (R8)
-	MOVQ AX, val+8(FP)
-	MOVQ DX, val+16(FP)
+	MOVQ AX, val_0+8(FP)
+	MOVQ DX, val_1+16(FP)
 	RET
 
 TEXT ·loadSCQNodeUint64(SB),NOSPLIT,$0
