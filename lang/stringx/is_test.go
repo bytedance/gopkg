12d6ece79ddda739232dc0ee76580e9f1e76017b
kaixiang zhong <xiang.ace@gmail.com>
2021-07-09 14:34:39 +0800

    fix(lang/stringx): check empty string on IsAlpha/IsNumeric/IsAlphanumeric. (#66)
    Co-authored-by: zhongkaixiang.kaka <zhongkaixiang.kaka@bytedance.com>

--- lang/stringx/is_test.go
+++ lang/stringx/is_test.go
@@ -23,13 +23,21 @@ import (
 func TestIs(t *testing.T) {
 	is := assert.New(t)
 
+	is.False(IsNumeric(""))
+	is.False(IsNumeric("  "))
 	is.False(IsNumeric(" bob "))
 	is.True(IsNumeric("123"))
 
+	is.False(IsAlpha(""))
+	is.False(IsAlpha(" "))
+	is.False(IsAlpha(" Voa "))
 	is.False(IsAlpha("123"))
 	is.True(IsAlpha("Voa"))
 	is.True(IsAlpha("bròwn"))
 
+	is.False(IsAlphanumeric(""))
+	is.False(IsAlphanumeric(" "))
+	is.False(IsAlphanumeric(" Voa "))
 	is.True(IsAlphanumeric("Voa"))
 	is.True(IsAlphanumeric("123"))
 	is.True(IsAlphanumeric("v123oa"))
