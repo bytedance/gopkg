0484c273777a9ee34d8d29f0ec3cdcf024ddd90f
zhangyunhao <zhangyunhao@bytedance.com>
2021-09-03 11:33:26 +0800

    fix govet warning

--- collection/lscq/asm.s
+++ collection/lscq/asm.s
@@ -18,10 +18,10 @@
 
 TEXT ·compareAndSwapUint128(SB),NOSPLIT,$0
 	MOVQ addr+0(FP), R8
-	MOVQ old_0+8(FP), AX
-	MOVQ old_1+16(FP), DX
-	MOVQ new_0+24(FP), BX
-	MOVQ new_1+32(FP), CX
+	MOVQ old1+8(FP), AX
+	MOVQ old2+16(FP), DX
+	MOVQ new1+24(FP), BX
+	MOVQ new2+32(FP), CX
 	LOCK
 	CMPXCHG16B (R8)
 	SETEQ swapped+40(FP)
