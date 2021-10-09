db544e15bc67c4d725b4c6554abe247290c9d999
zhangyunhao <zhangyunhao@bytedance.com>
2021-10-09 17:42:02 +0800

    fix

--- collection/lscq/asm.go
+++ collection/lscq/asm.go
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//go:build amd64 && !gccgo && !appengine
 // +build amd64,!gccgo,!appengine
 
 package lscq
