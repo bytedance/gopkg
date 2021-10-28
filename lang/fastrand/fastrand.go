3c2bb7f332daf32b041f03f17a4fd812e701a098
liusong <1736912420@qq.com>
2021-10-28 17:00:03 +0800

    feat(fastrand): Add some condition judgment

--- lang/fastrand/fastrand.go
+++ lang/fastrand/fastrand.go
@@ -54,6 +54,9 @@ func Int63n(n int64) int64 {
 	if n <= 0 {
 		panic("invalid argument to Int63n")
 	}
+	if n<=1<<32-1{
+		return int64(Int31n(int32(n)))
+	}
 	if n&(n-1) == 0 { // n is power of two, can mask
 		return Int63() & (n - 1)
 	}
@@ -73,6 +76,9 @@ func Int31n(n int32) int32 {
 	// EQ
 	if n <= 0 {
 		panic("invalid argument to Int31n")
+	}
+	if n&(n-1) == 0 {       // n is power of two, can mask
+		return Int31() & (n - 1)
 	}
 	v := Uint32()
 	prod := uint64(v) * uint64(n)
@@ -116,11 +122,23 @@ func Float32() float32 {
 func Uint32n(n uint32) uint32 {
 	// This is similar to Uint32() % n, but faster.
 	// See https://lemire.me/blog/2016/06/27/a-fast-alternative-to-the-modulo-reduction/
+	if n <= 0 {
+		panic("invalid argument to Uint32n")
+	}
+	if n&(n-1) == 0 {                // n is power of two, can mask
+		return Uint32() & (n - 1)
+	}
 	return uint32(uint64(Uint32()) * uint64(n) >> 32)
 }
 
 // Uint64n returns a pseudo-random number in [0,n).
 func Uint64n(n uint64) uint64 {
+	if n <= 0 {
+		panic("invalid argument to Uint64n")
+	}
+	if n<=1<<32-1 {
+		return uint64(Uint32n(uint32(n)))
+	}
 	return Uint64() % n
 }
 
