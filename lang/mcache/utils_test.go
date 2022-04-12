e37ba8d112e8592c3e21e9102186de406732ea48
Hchen <hechen@bytedance.com>
2022-04-12 18:16:26 +0800

    optimize: use bits.Len64 instead of bsr

--- lang/mcache/utils_test.go
+++ lang/mcache/utils_test.go
@@ -26,3 +26,13 @@ func TestBsr(t *testing.T) {
 	assert.Equal(t, bsr((1<<10)-1), 9)
 	assert.Equal(t, bsr((1<<30)+(1<<19)+(1<<16)+(1<<1)), 30)
 }
+
+func BenchmarkBsr(b *testing.B) {
+	num := (1 << 30) + (1 << 19) + (1 << 16) + (1 << 1)
+
+	b.ReportAllocs()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		_ = bsr(num + i)
+	}
+}
