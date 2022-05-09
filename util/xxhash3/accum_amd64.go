c41b8bdf314bebd7af386f5412f65446fb288895
zhangyunhao <zhangyunhao116@gmail.com>
2022-05-09 11:55:21 +0000

    .

--- util/xxhash3/accum_amd64.go
+++ util/xxhash3/accum_amd64.go
@@ -21,10 +21,10 @@ func accumSSE2(acc *[8]uint64, xinput, xsecret unsafe.Pointer, len uintptr)
 
 func accum(xacc *[8]uint64, xinput, xsecret unsafe.Pointer, l uintptr) {
 	if avx2 {
-		accumAVX2(&xacc, xinput, xsecret, length)
+		accumAVX2(xacc, xinput, xsecret, l)
 	} else if sse2 {
-		accumSSE2(&xacc, xinput, xsecret, length)
+		accumSSE2(xacc, xinput, xsecret, l)
 	} else {
-		accumScalar(&xacc, xinput, xsecret, length)
+		accumScalar(xacc, xinput, xsecret, l)
 	}
 }
