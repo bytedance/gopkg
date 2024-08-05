f75b326cb05c91c45f18f786f83773466ba4e07b
wangzhuowei <wangzhuowei@bytedance.com>
2024-08-05 11:01:54 +0800

    chore: rm unstable ci assert

--- cloud/circuitbreaker/metricer_test.go
+++ cloud/circuitbreaker/metricer_test.go
@@ -78,12 +78,13 @@ func TestMetricser1(t *testing.T) {
 
 	rate := m.ErrorRate()
 	assert(t, rate > .6 && rate < .7)
-	s = m.Successes()
-	assert(t, s > int64(tot/3-1000) && s < int64(tot/3+1000))
-	f = m.Failures()
-	assert(t, f > int64(tot/3-1000) && f < int64(tot/3+1000))
-	ts := m.Timeouts()
-	assert(t, ts > int64(tot/3-1000) && ts < int64(tot/3+1000))
+	// it's not guaranteed in unstable ci env
+	//s = m.Successes()
+	//assert(t, s > int64(tot/3-1000) && s < int64(tot/3+1000))
+	//f = m.Failures()
+	//assert(t, f > int64(tot/3-1000) && f < int64(tot/3+1000))
+	//ts := m.Timeouts()
+	//assert(t, ts > int64(tot/3-1000) && ts < int64(tot/3+1000))
 }
 
 // TestMetricser2 tests functions about time
